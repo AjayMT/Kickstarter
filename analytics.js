f621b1f0126880ae9a36992127dff12c0df217f5
AjayMT <ajay.tatachar@gmail.com>
2013-11-29 16:53:05 +0530

    Add analytics stuff.

---
+++ analytics.js
@@ -0,0 +1,10 @@
+const KS_VERSION = '1.6.2';
+
+function pageView () {
+  mixpanel.track('Page view');
+}
+
+function download () {
+  mixpanel.track('Download');
+  window.location.href = '/downloads/Kickstarter-' + KS_VERSION + '.zip';
+}
